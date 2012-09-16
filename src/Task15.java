0b27b4cc469aa1c1c2f3ec9882eafcfc3827a634
Denis Karyakin <samhane.me@gmail.com>
2012-09-16 23:13:05 +0400

    Working 15 task

--- src/Task15.java
+++ src/Task15.java
@@ -19,13 +19,27 @@ public class Task15 extends CrypthText {
 
     protected void createResultText() {
         StringBuilder workSecret = new StringBuilder();
+        for (int i = 0; i < sourceText.length(); i++) {
+            if (encode) {
+                workSecret.append(this.strings[1].charAt(this.strings[0].indexOf(sourceText.charAt(i))));
+            } else {
+                workSecret.append(this.strings[0].charAt(this.strings[1].indexOf(sourceText.charAt(i))));
+            }
+        }
         resultText.add(workSecret.toString());
     }
 
     private void createStrings() {
         this.strings[0] = this.alphabet;
+        this.strings[1] = "";
         StringBuilder twoRow = new StringBuilder();
-        strings[1] = key;
+
+        for (int i = 0; i < key.length(); i++) {
+            if (!strings[1].contains(String.valueOf(key.charAt(i)))) {
+                strings[1] = strings[1].concat(String.valueOf(key.charAt(i)));
+            }
+        }
+
         for (int i = 0; i < alphabet.length(); i++) {
             if (!strings[1].contains(String.valueOf(alphabet.charAt(i)))) {
                 twoRow.append(alphabet.charAt(i));
